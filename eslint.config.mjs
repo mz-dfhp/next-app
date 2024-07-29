21477e157ab9651991b23a64b4e15f5f4c302269
lc <486836332@qq.com>
2024-07-29 18:39:28 +0800

    feat: init

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  react: true,
+  formatters: {
+    css: true,
+    html: true,
+    markdown: true,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+  },
+})
