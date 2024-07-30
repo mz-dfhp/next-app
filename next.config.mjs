5c211f93204c238f94a19e43ef7143de2f00a78c
lc <486836332@qq.com>
2024-07-30 19:27:24 +0800

    feat: update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import process from 'node:process'
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'github' ? '/next-app' : '',
+  assetPrefix: process.env.NODE_ENV === 'github' ? '/next-app/' : '',
+  trailingSlash: process.env.NODE_ENV === 'github',
+}
 
 export default nextConfig
